6f8e8cef8b06d48e097e73fe18b6f62afdb50fcc
seoyeonko <seoyeon99o@naver.com>
2022-11-24 19:12:25 +0900

    feat: 예외처리

--- server/routes/todo.js
+++ server/routes/todo.js
@@ -9,83 +9,94 @@ router.get('/', (req, res) => {
 
 // GET /todos - show all todos (READ)
 router.get('/todos', async (req, res) => {
-  let todos = await Todo.findAll();
-
-  res.send(todos);
+  try {
+    let todos = await Todo.findAll();
+    res.send(todos);
+  } catch (err) {
+    res.send(err);
+  }
 });
 
 // GET /todo/:todoId - show a specific todo (READ ONE)
 router.get('/todo/:todoId', async (req, res) => {
-  let todo = await Todo.findOne({
-    where: {
-      id: { [Op.eq]: req.params.todoId },
-    },
-    // raw: true,
-  });
-
-  // 조회 결과 없음
-  if (todo === null) {
-    return res.send(false);
-  }
-
-  res.send(todo);
-});
-
-// POST /todo - create a new todo (CREATE)
-router.post('/todo', async (req, res) => {
-  console.log(req.body);
-  let newTodo = await Todo.create({
-    todo: req.body.todo,
-    completed: req.body.completed,
-  });
-
-  res.send(newTodo);
-});
-
-// PATCH /todo/:todoId - edit a specific todo (UPDATE)
-router.patch('/todo/:todoId', async (req, res) => {
-  // 배열 구조 분해
-  // [isUpdated] = [ 0 ] or [ 1 ]
-  let [idUpdated] = await Todo.update(
-    {
-      todo: req.body.todo,
-      completed: req.body.completed,
-    },
-    {
+  try {
+    let todo = await Todo.findOne({
       where: {
         id: { [Op.eq]: req.params.todoId },
       },
       // raw: true,
+    });
+    // 조회 결과 없음
+    if (todo === null) {
+      return res.send(false);
     }
-  );
-  // console.log(idUpdated); // 0 or 1
-
-  // 수정 실패
-  if (idUpdated === 0) {
-    return res.send(false);
+    res.send(todo);
+  } catch (err) {
+    res.send(err);
   }
+});
 
-  // 수정 성공
-  res.send(true);
+// POST /todo - create a new todo (CREATE)
+router.post('/todo', async (req, res) => {
+  try {
+    let newTodo = await Todo.create({
+      todo: req.body.todo,
+      completed: req.body.completed,
+    });
+    res.send(newTodo);
+  } catch (err) {
+    res.send(err);
+  }
+});
+
+// PATCH /todo/:todoId - edit a specific todo (UPDATE)
+router.patch('/todo/:todoId', async (req, res) => {
+  try {
+    // 배열 구조 분해
+    // [isUpdated] = [ 0 ] or [ 1 ]
+    let [idUpdated] = await Todo.update(
+      {
+        todo: req.body.todo,
+        completed: req.body.completed,
+      },
+      {
+        where: {
+          id: { [Op.eq]: req.params.todoId },
+        },
+        // raw: true,
+      }
+    );
+    // console.log(idUpdated); // 0 or 1
+    // 수정 실패
+    if (idUpdated === 0) {
+      return res.send(false);
+    }
+    // 수정 성공
+    res.send(true);
+  } catch (err) {
+    res.send(err);
+  }
 });
 
 // DELETE /todo/:todoId - remove a specific todo (DELETE)
 router.delete('/todo/:todoId', async (req, res) => {
-  let isDeleted = await Todo.destroy({
-    where: {
-      id: { [Op.eq]: req.params.todoId },
-    },
-    raw: true,
-  });
-  // console.log(isDeleted); // 0 or 1
-
-  // 삭제 실패
-  if (!isDeleted) {
-    return res.send(false);
+  try {
+    let isDeleted = await Todo.destroy({
+      where: {
+        id: { [Op.eq]: req.params.todoId },
+      },
+      raw: true,
+    });
+    // console.log(isDeleted); // 0 or 1
+    // 삭제 실패
+    if (!isDeleted) {
+      return res.send(false);
+    }
+    // 삭제 성공
+    res.send(true);
+  } catch (err) {
+    res.send(err);
   }
-
-  // 삭제 성공
-  res.send(true);
 });
 
 module.exports = router;
