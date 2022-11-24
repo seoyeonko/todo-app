6cf6e06ebabcc4bf2353162fea096535717d2612
seoyeonko <seoyeon99o@naver.com>
2022-11-25 00:56:21 +0900

    feat: cors 설정

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = 8000;
 
 app.use('/static', express.static(__dirname + '/static'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 
 const todoRouter = require('./routes/todo');
 app.use('/api', todoRouter); // 기본주소: localhost:PORT/api
